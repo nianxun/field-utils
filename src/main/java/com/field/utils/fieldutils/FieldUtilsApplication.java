7fdce08574a4bdd810494a0c329ec17f5401d9a6
field <1914479674@qq.com>
2021-07-27 17:08:54 +0800

    First commit

---
+++ src/main/java/com/field/utils/fieldutils/FieldUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.field.utils.fieldutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FieldUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/field/utils/fieldutils/FieldUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.field.utils.fieldutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
