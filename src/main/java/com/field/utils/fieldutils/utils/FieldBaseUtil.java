3d459f9c0ca39b0f2fa69d71d0871d68a20f2a60
field <z_w_field@163.com>
2023-10-11 22:07:13 +0800

    feat: 新增图片对比的工具

--- src/main/java/com/field/utils/fieldutils/utils/FieldBaseUtil.java
+++ src/main/java/com/field/utils/fieldutils/utils/FieldBaseUtil.java
@@ -1,10 +1,9 @@
 package com.field.utils.fieldutils.utils;
 
 import net.redhogs.cronparser.CronExpressionDescriptor;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
 
-import java.io.ByteArrayInputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectStreamClass;
 import java.text.ParseException;
 import java.util.Locale;
 
@@ -41,6 +40,17 @@ public class FieldBaseUtil {
      */
     public static String cronToString(String text) throws ParseException {
         return CronExpressionDescriptor.getDescription(text, Locale.CHINESE);
+    }
+
+    /**
+     * 清除富文本中指定的标签，以防止安全问题
+     * @param payload 输入的文本
+     * @return 处理结果
+     */
+    public static String safeCleanHtml(String payload){
+        //String payload = "aaa<script>alert(1)</script>bbb<img src=\"http://.cc/a.jpg\" class=\"s\" onerror=\"alert(1)\"><a href=\"http://.cc/test\">sss</a>";
+        Safelist safelist = Safelist.basicWithImages().addAttributes(":all", "class");
+        return Jsoup.clean(payload, safelist);
     }
 
 }
