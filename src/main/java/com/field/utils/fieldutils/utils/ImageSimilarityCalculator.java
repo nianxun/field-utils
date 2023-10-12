ea6d5b74d5fd1275dbb45ed2d7095b06b16bc181
field <z_w_field@163.com>
2023-10-12 17:10:16 +0800

    feat: 优化代码

--- src/main/java/com/field/utils/fieldutils/utils/ImageSimilarityCalculator.java
+++ src/main/java/com/field/utils/fieldutils/utils/ImageSimilarityCalculator.java
@@ -4,10 +4,6 @@ import org.bytedeco.opencv.global.opencv_core;
 import org.bytedeco.opencv.global.opencv_imgcodecs;
 import org.bytedeco.opencv.global.opencv_imgproc;
 import org.bytedeco.opencv.opencv_core.Mat;
-import org.opencv.core.Core;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-
 /**
  * @author Field
  * @date 2023-10-11 17:55
@@ -16,13 +12,13 @@ public class ImageSimilarityCalculator {
 
     public static double calculateSimilarity(String imagePath1, String imagePath2) {
         // 读取图片
-        org.bytedeco.opencv.opencv_core.Mat image1 = opencv_imgcodecs.imread(imagePath1, Imgcodecs.IMREAD_COLOR);
-        org.bytedeco.opencv.opencv_core.Mat image2 = opencv_imgcodecs.imread(imagePath2, Imgcodecs.IMREAD_COLOR);
+        Mat image1 = opencv_imgcodecs.imread(imagePath1, opencv_imgcodecs.IMREAD_COLOR);
+        Mat image2 = opencv_imgcodecs.imread(imagePath2, opencv_imgcodecs.IMREAD_COLOR);
         // 转换为HSV颜色空间
-        org.bytedeco.opencv.opencv_core.Mat hsvImage1 = new org.bytedeco.opencv.opencv_core.Mat();
-        org.bytedeco.opencv.opencv_core.Mat hsvImage2 = new org.bytedeco.opencv.opencv_core.Mat();
-        opencv_imgproc.cvtColor(image1, hsvImage1, Imgproc.COLOR_BGR2HSV);
-        opencv_imgproc.cvtColor(image2, hsvImage2, Imgproc.COLOR_BGR2HSV);
+        Mat hsvImage1 = new Mat();
+        Mat hsvImage2 = new Mat();
+        opencv_imgproc.cvtColor(image1, hsvImage1, opencv_imgproc.COLOR_BGR2HSV);
+        opencv_imgproc.cvtColor(image2, hsvImage2, opencv_imgproc.COLOR_BGR2HSV);
 
         // 划分颜色空间
         int[] channels = {0, 1};
@@ -32,13 +28,13 @@ public class ImageSimilarityCalculator {
         float[][] ranges = {hueRange, saturationRange};
 
         // 计算直方图
-        org.bytedeco.opencv.opencv_core.Mat histImage1 = new org.bytedeco.opencv.opencv_core.Mat();
-        org.bytedeco.opencv.opencv_core.Mat histImage2 = new org.bytedeco.opencv.opencv_core.Mat();
+        Mat histImage1 = new Mat();
+        Mat histImage2 = new Mat();
         opencv_imgproc.calcHist(
                 hsvImage1,
                 1,
                 channels,
-                new org.bytedeco.opencv.opencv_core.Mat(),
+                new Mat(),
                 histImage1,
                 1,
                 histSize,
@@ -48,18 +44,18 @@ public class ImageSimilarityCalculator {
                 hsvImage2,
                 1,
                 channels,
-                new org.bytedeco.opencv.opencv_core.Mat(),
+                new Mat(),
                 histImage2,
                 1,
                 histSize,
                 saturationRange
         );
         // 归一化直方图
-        opencv_core.normalize(histImage1, histImage1, 0, 1, Core.NORM_MINMAX, -1, new org.bytedeco.opencv.opencv_core.Mat());
-        opencv_core.normalize(histImage2, histImage2, 0, 1, Core.NORM_MINMAX, -1, new Mat());
+        opencv_core.normalize(histImage1, histImage1, 0, 1, opencv_core.NORM_MINMAX, -1, new Mat());
+        opencv_core.normalize(histImage2, histImage2, 0, 1, opencv_core.NORM_MINMAX, -1, new Mat());
         // 计算直方图相似度
         opencv_imgproc.contourArea(histImage1);
-        return opencv_imgproc.compareHist(histImage1, histImage2, Imgproc.CV_COMP_CORREL);
+        return opencv_imgproc.compareHist(histImage1, histImage2, opencv_imgproc.CV_COMP_CORREL);
     }
 
 }
