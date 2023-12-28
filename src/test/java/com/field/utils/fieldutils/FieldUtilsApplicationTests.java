210117431aebd12a0ecf9bce114b8b974606e818
field <z_w_field@163.com>
2023-12-28 17:16:53 +0800

    feat: 增加一个目录
    记一下写的文档，写过很多东西确实没有记下来，以前没有感觉，现在总感觉有些可惜。

--- src/test/java/com/field/utils/fieldutils/FieldUtilsApplicationTests.java
+++ src/test/java/com/field/utils/fieldutils/FieldUtilsApplicationTests.java
@@ -1,21 +1,21 @@
 package com.field.utils.fieldutils;
 
-import com.field.utils.fieldutils.utils.FieldBaseUtil;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.text.ParseException;
 
 @SpringBootTest
 class FieldUtilsApplicationTests {
 
     @Test
     void contextLoads() {
-        try {
-            System.out.println(FieldBaseUtil.cronToString("0 0 6 * * ?"));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+//        Loader.load(opencv_java.class);
+//        String templateFilePath = "D:\\work\\t.png";
+//        String originalFilePath = "D:\\work\\5.png";
+//        //读取图片文件
+//        Mat templateImage = Imgcodecs.imread(templateFilePath, Imgcodecs.IMREAD_COLOR);
+//        Mat originalImage = Imgcodecs.imread(originalFilePath, Imgcodecs.IMREAD_COLOR);
+//        ImageRecognition.matchImage(templateImage, originalImage);
     }
 
 }
